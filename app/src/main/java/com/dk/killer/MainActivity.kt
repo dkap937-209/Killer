f312089418942e7aff0e866e03a97498f993e06d
dkap937-209 <67822005+dkap937-209@users.noreply.github.com>
2023-02-08 21:57:03 +1300

    Designed layout file

--- app/src/main/java/com/dk/killer/MainActivity.kt
+++ app/src/main/java/com/dk/killer/MainActivity.kt
@@ -2,8 +2,11 @@ package com.dk.killer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dk.killer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
